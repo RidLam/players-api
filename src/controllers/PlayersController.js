185b45d288f544c397d549dd0fb5afb05b376944
lamghari ridouan <lamghari.ridouan@gmail.com>
2022-01-08 19:58:00 +0100

    Add API: getAllPlayers and getPlayerById

--- src/controllers/PlayersController.js
+++ src/controllers/PlayersController.js
@@ -1,9 +1,48 @@
 const axios = require('axios')
 
 const getAllPlayers = (async (req, res) => {
-    res.status(200).json("Hello world")
+  try {
+    const players = await getPlayers();
+    res.status(200).json(players)
+  } catch (error) {
+    res.status(402).json(error)
+  }
 })
 
+const getPlayerById = (async (req, res) => {
+  try {
+    const { player_id } = req.params;
+    const players = await getPlayers();
+    const player = players.find(_player => _player.id === parseInt(player_id));
+    if (player) {
+      res.status(200).json(player);
+    } else {
+      throw `Sorry, player with id : ${player_id} not found`;
+    }
+    
+  } catch (error) {
+    res.status(402).json(error);
+  }
+  
+})
+
+const getPlayers = () =>
+  new Promise((resolve, reject) => {
+    axios.get(process.env.EXTERNAL_PLAYERS_API)
+    .then(result => {
+      if (result && result.data && result.data.players) {
+        resolve(result.data.players);
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(error => {
+      reject(error);
+    });
+  })
+
+
 module.exports = {
-    getAllPlayers
+    getAllPlayers,
+    getPlayerById
 }
--- src/routes/PlayersRoute.js
+++ src/routes/PlayersRoute.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const  { 
-    getAllPlayers
+    getAllPlayers,
+    getPlayerById
 } = require('../controllers/PlayersController.js')
 
 router.get('/', getAllPlayers);
 
+router.get('/:player_id', getPlayerById)
 
 module.exports = router
